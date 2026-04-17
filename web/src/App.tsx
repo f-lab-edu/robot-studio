c9fc685264becb197340861b7df1993a39ff627d
GomSon-E <gomsonixx@gmail.com>
2026-04-17 21:23:33 +0900

    REFACTOR: Update routes - merge episode route into dataset detail with query param redirect

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import DatasetListPage from "./pages/DatasetListPage";
 import DatasetDetailPage from "./pages/DatasetDetailPage";
-import EpisodeDetailPage from "./pages/EpisodeDetailPage";
+
+function EpisodeRedirect() {
+  const { name, idx } = useParams<{ name: string; idx: string }>();
+  return (
+    <Navigate
+      to={`/datasets/${encodeURIComponent(name ?? "")}?episode=${idx ?? "0"}`}
+      replace
+    />
+  );
+}
 
 function App() {
   return (
@@ -14,7 +23,7 @@ function App() {
         <Route path="/" element={<Navigate to="/datasets" replace />} />
         <Route path="/datasets" element={<DatasetListPage />} />
         <Route path="/datasets/:name" element={<DatasetDetailPage />} />
-        <Route path="/datasets/:name/episodes/:idx" element={<EpisodeDetailPage />} />
+        <Route path="/datasets/:name/episodes/:idx" element={<EpisodeRedirect />} />
         <Route path="*" element={<Navigate to="/datasets" replace />} />
       </Routes>
     </BrowserRouter>
