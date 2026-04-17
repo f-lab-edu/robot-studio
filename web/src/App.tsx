7624ae276e4d4af24157ab742d5d0fa6081f6ce5
GomSon-E <gomsonixx@gmail.com>
2026-04-17 20:29:30 +0900

    BUILD: Add Dataset Viewer Routes to App Router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import DatasetListPage from "./pages/DatasetListPage";
+import DatasetDetailPage from "./pages/DatasetDetailPage";
+import EpisodeDetailPage from "./pages/EpisodeDetailPage";
 
 function App() {
   return (
@@ -8,7 +11,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/datasets" replace />} />
+        <Route path="/datasets" element={<DatasetListPage />} />
+        <Route path="/datasets/:name" element={<DatasetDetailPage />} />
+        <Route path="/datasets/:name/episodes/:idx" element={<EpisodeDetailPage />} />
+        <Route path="*" element={<Navigate to="/datasets" replace />} />
       </Routes>
     </BrowserRouter>
   );
